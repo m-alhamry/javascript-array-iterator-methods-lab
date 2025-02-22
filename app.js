58b92380d3e747f5071f826bb3109ee7f518072e
Mohamed Alhamry <alhamrimohammed4@email.com>
2025-02-22 18:12:09 +0300

    Exercise 2 Level Up

--- app.js
+++ app.js
@@ -374,9 +374,51 @@ let totalYearsLived = 0;
 
 // Complete the exercise in the space below:
 const initialValue = 0;
-totalYearsLived = inventors.reduce((accumulator, currentValue) =>
-    accumulator + (currentValue.passed - currentValue.year), initialValue,
-);
+totalYearsLived = inventors.reduce((accumulator, currentValue) => {
+    return accumulator + (currentValue.passed - currentValue.year);
+}, initialValue);
 // Check your work:
 console.log('Level Up 1 my result: ', totalYearsLived);
 console.log('Level Up 1 correct result: ', 861);
+
+/*
+Level Up exercise 2: Array.prototype.reduce()
+
+Tallying travel methods using Array.prototype.reduce(). 
+
+Count the number of times each travel method appears in the 'travelMethods'
+array.
+
+- The resulting object should have keys as the travel methods 
+  ('car', 'truck', 'bike', etc.) and values as their respective counts.
+- Store this object in the variable 'travelMethodCounts'.
+
+Hints:
+- Inside the reduce function, check if the travel method already exists as a key
+  in your accumulator object. If it does, increment its count. If not, add it 
+  to the object and give it a value of 1.
+- Since you want to return an object, be sure to pass an empty {} for the 
+  initial value of the "accumulator".
+*/
+console.log('\n===================== Level Up exercise 2 =====================');
+let travelMethodCounts = {};
+
+// Complete the exercise in the space below:
+const initialValue2 = {};
+travelMethodCounts = travelMethods.reduce((accumulator, currentValue) => {
+    if(!accumulator[currentValue]) {
+        accumulator[currentValue] = 1;
+    } else {
+        accumulator[currentValue]++;
+    }
+    return accumulator;
+}, initialValue2);
+// Check your work:
+console.log('Level Up 2 my result: ', travelMethodCounts);
+console.log('Level Up 2 correct result: ', {
+    car: 5,
+    truck: 3,
+    bike: 2,
+    walk: 2,
+    van: 2,
+});
